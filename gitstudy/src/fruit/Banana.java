f944ecce3e53164c3e893653d0fa23233dbead9a
Gi7E-36 <Gi7E-36@Gi7A-17>
2023-11-13 11:57:37 +0900

    get, set add

--- gitstudy/src/fruit/Banana.java
+++ gitstudy/src/fruit/Banana.java
@@ -1,5 +1,21 @@
 package fruit;
 
 public class Banana {
-
+	int price;
+	int qty;
+	
+	public int getPrice() {
+		return price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
 }
+
+
